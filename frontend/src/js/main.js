780e9cf61866d7394593f44c5646948df201ad37
Minh Pham <minh.pham@3forcom.com>
2021-03-31 16:45:21 +0700

    :boom: feat(landing-page): implement sections

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,121 +1,68 @@
-/* global SimpleBar */
-function ui() {
-    // Select UI
-    $.fn.select2.defaults.set('width', '100%');
-    $('.select-ui').each(function() {
-        const el = $(this);
-        const selectUI = el.select2({
-            placeholder: el.data('placeholder')
-        });
+/* global WOW */
+(function($) {
+    function ui() {
+        new WOW().init();
+    }
+    function initScrollSmoothly() {
+        new SmoothScroll(document, 120, 12);
+    }
 
-        // Update UI Scroll - Open dropdown
-        selectUI.on('select2:open', function() {
-            const id = $('.select2-results  > .select2-results__options').attr('id');
-            $('.select2-results')
-                .attr({ id: id + '-group' })
-                .queue(function(next) {
-                    new SimpleBar($('#' + id + '-group')[0]);
-                    next();
-                });
-        });
-    });
+    function SmoothScroll(target, speed, smooth) {
+        if (target === document) target = document.scrollingElement || document.documentElement || document.body.parentNode || document.body; // cross browser support for document scrolling
 
-    // Range UI
-    $('.range-ui').each(function(key) {
-        const el = $(this);
-        el.attr({ id: 'range-ui-' + key }).queue(function(next) {
-            $('#range-ui-' + key).ionRangeSlider();
-            next();
-        });
-    });
+        let moving = false;
+        let pos = target.scrollTop;
+        const frame = target === document.body && document.documentElement ? document.documentElement : target; // safari is the new IE
 
-    // Scroll
-    $('.scroll-ui').each(function(key) {
-        const el = $(this);
-        el.attr({ id: 'scroll-ui-' + key }).queue(function(next) {
-            new SimpleBar($('#' + el.attr('id'))[0]);
-            next();
-        });
-    });
+        target.addEventListener('mousewheel', scrolled, { passive: false });
+        target.addEventListener('DOMMouseScroll', scrolled, { passive: false });
 
-    // File Browse UI
-    $('.file-ui .file-ui-input').change(function(e) {
-        if (typeof e.target.files[0] !== 'undefined') {
-            const fileName = e.target.files[0].name;
-            $(this)
-                .siblings('.file-ui-label')
-                .text(fileName);
+        function scrolled(e) {
+            e.preventDefault(); // disable default scrolling
+
+            const delta = normalizeWheelDelta(e);
+
+            pos += -delta * speed;
+            pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)); // limit scrolling
+
+            if (!moving) update();
         }
+
+        function normalizeWheelDelta(e) {
+            if (e.detail) {
+                if (e.wheelDelta) return (e.wheelDelta / e.detail / 40) * (e.detail > 0 ? 1 : -1);
+                // Opera
+                else return -e.detail / 3; // Firefox
+            } else return e.wheelDelta / 120; // IE,Safari,Chrome
+        }
+
+        function update() {
+            moving = true;
+
+            const delta = (pos - target.scrollTop) / smooth;
+
+            target.scrollTop += delta;
+
+            if (Math.abs(delta) > 0.5) requestFrame(update);
+            else moving = false;
+        }
+
+        const requestFrame = (function() {
+            // requestAnimationFrame cross browser
+            return (
+                window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function(func) {
+                    window.setTimeout(func, 1000 / 50);
+                }
+            );
+        })();
+    }
+    $(function() {
+        ui();
+        initScrollSmoothly();
     });
-
-    // Parallax
-    $('[data-paroller-factor]').paroller();
-}
-
-// Image svg
-function imgSVG() {
-    $('img.svg').each(function() {
-        const $img = $(this);
-        const imgID = $img.attr('id');
-        const imgClass = $img.attr('class');
-        const imgURL = $img.attr('src');
-
-        $.get(
-            imgURL,
-            function(data) {
-                // Get the SVG tag, ignore the rest
-                let $svg = $(data).find('svg');
-
-                // Add replaced image's ID to the new SVG
-                if (typeof imgID !== 'undefined') $svg = $svg.attr('id', imgID);
-
-                // Add replaced image's classes to the new SVG
-                if (typeof imgClass !== 'undefined') $svg = $svg.attr('class', imgClass + ' replaced-svg');
-
-                // Remove any invalid XML tags as per http://validator.w3.org
-                $svg = $svg.removeAttr('xmlns:a');
-
-                // Replace image with new SVG
-                $img.replaceWith($svg);
-            },
-            'xml'
-        );
-    });
-}
-
-function gotoTop() {
-    const topTop = $('.toTop');
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) topTop.addClass('active');
-        else topTop.removeClass('active');
-    });
-    topTop.click(function() {
-        $('body,html').animate(
-            {
-                scrollTop: 0
-            },
-            500
-        );
-        return false;
-    });
-}
-
-function init() {
-    // Base
-    // ui();
-    // Image SVG
-    imgSVG();
-    // Go to top
-    gotoTop();
-
-    // $(window).on("debouncedresize", function (event) {
-    //     // ...
-    // });
-    if (1 == 2) ui();
-}
-
-$('body').imagesLoaded(function() {
-    init();
-    $('body').addClass('loaded');
-    $('.pageLoad').fadeOut();
-});
+})(jQuery);
