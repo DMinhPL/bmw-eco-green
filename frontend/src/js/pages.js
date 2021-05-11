08cd7f24ff54c0f47377cf35fc6d7ca695e27b52
Minh Pham <minh.pham@3forcom.com>
2021-05-11 11:05:51 +0700

    :bug: feat(landingpage2): add youtube video to story

--- frontend/src/js/pages.js
+++ frontend/src/js/pages.js
@@ -12,12 +12,27 @@
                     $section.removeClass('active');
                     $it.addClass('active');
                     $background.removeClass('active');
-                    $(`.s-story_img-item[data-thumb='${id}']`).addClass('active');
+                    const $item = $(`.s-story_img-item[data-thumb='${id}']`);
+                    $item.addClass('active');
                 }
             });
         });
+        if ($('.s-story_img-item.hasVideo').find('iframe').length > 0) {
+            const $video = $('.s-story_img-item.hasVideo').find('iframe');
+            function youtube_parser(url) {
+                const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+                const match = url.match(regExp);
+                return match && match[7].length == 11 ? match[7] : false;
+            }
+            $('.s-story_img-item.hasVideo').find('iframe')[0].src += `&autoplay=1&controls=0&mute=1&loop=1&playlist=${youtube_parser($video[0].src)}`;
+        }
+    }
+
+    function autoScrollTop() {
+        $('body, html').animate({ scrollTop: 0 }, 800);
     }
     $(function() {
         scrollIntoView();
+        autoScrollTop();
     });
 })(jQuery);
